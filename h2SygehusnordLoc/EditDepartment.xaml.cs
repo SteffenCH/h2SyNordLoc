8ff70f9ef58ee4147f269436be00a62cbf183917
Kris Riisgaard Larsen <kris7011@elev.techcollege.dk>
2017-08-30 09:37:40 +0200

    Added Functionality to Delete function and more

--- h2SygehusnordLoc/EditDepartment.xaml.cs
+++ h2SygehusnordLoc/EditDepartment.xaml.cs
@@ -64,11 +64,6 @@ namespace h2SygehusnordLoc
         {
             this.Close();
         }
-
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-        }
     }
 }
 
